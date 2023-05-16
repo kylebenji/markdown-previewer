60cb31910cc334f23ce87c4471c3ad7e41304d07
kylebenji <127806283+kylebenji@users.noreply.github.com>
2023-05-16 08:58:00 -0700

    initial code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Markdown Previewer
+
+## Features
+
+- Takes GitHub markdown and parses it into displayable formatted html
+
+## To Do
+
+- make the page look nicer
+- make sure line breaks work properly
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+const initialMarkdown = `# Preview
+Welcome to the markdown previewer, enter any valid GitHub style markdown here to see it in html on the preview on the right
+
+## \`Code\` blocks
+\`\`\`
+let codeblock = "\`\`\`"
+\`\`\`
+
+See a great survey of **GitHub** markdown [here](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax)
+
+## Why use it?
+- see markdown as you write it
+- satiate your curiosity
+- breath easy knowing that computers will do work for you
+
+## Reviews from users
+> This tool is great!! I have never seen my html just appear while I wrote plain text like that!!
+![generic user](https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80)`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: initialMarkdown };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div id="app-area" className="m-auto col-8">
+        <h1 className="text-center">Markdown Preview</h1>
+        <div id="preview-area" className="row">
+          <textarea id="editor" onChange={this.handleChange} className="col-6">
+            {initialMarkdown}
+          </textarea>
+          <div id="preview" className="col-6">
+            <ReactMarkdown>{this.state.input}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
